ea25c347225e8904c7e32a5bb075b36fbb4f44a9
Erik Pearson <eapearson@lbl.gov>
2015-11-08 19:14:18 -0800

    debugging

--- src/kb/widget/widgetSet.js
+++ src/kb/widget/widgetSet.js
@@ -111,6 +111,8 @@ define([
                         message: 'The widget ' + rec.title + ' does not have a valid node at ' + rec.id
                     };
                 }
+                console.log('ATTACH');
+                console.log(rec);
                 return rec.widget.attach(rec.node);
             }));
         }
