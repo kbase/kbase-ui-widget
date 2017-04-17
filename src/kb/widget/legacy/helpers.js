b1a4e4275743dd30a19eb7005af386596c752eb9
Erik Pearson <eapearson@lbl.gov>
2017-04-17 12:05:03 -0700

    Remove commented out code that was upsetting uglify

--- src/kb/widget/legacy/helpers.js
+++ src/kb/widget/legacy/helpers.js
@@ -1,9 +1,10 @@
-/*global define*/
-/*jslint browser:true,white:true*/
 define([
     'jquery',
     'kb_common/html'
-], function ($, html) {
+], function (
+    $,
+    html
+) {
     'use strict';
     // jQuery plugins that you can use to add and remove a 
     // loading giff to a dom element.
@@ -15,24 +16,6 @@ define([
         $(this).rmLoading();
         // TODO: handle "big"
         $(this).append(div({ class: 'loader' }, html.loading(text)));
-        //        if (big) {
-        //            if (text !== undefined) {
-        //                $(this).append('<p class="text-center text-muted loader"><br>'+
-        //                     '<img src="assets/img/ajax-loader-big.gif"> '+text+'</p>');
-        //            } else {
-        //                $(this).append('<p class="text-center text-muted loader"><br>'+
-        //                     '<img src="assets/img/ajax-loader-big.gif"> loading...</p>');      
-        //            }
-        //        } else {
-        //            if (text !== 'undefined') {
-        //                $(this).append('<p class="text-muted loader">'+
-        //                     '<img src="assets/img/ajax-loader.gif"> '+text+'</p>');
-        //            } else {
-        //                $(this).append('<p class="text-muted loader">'+
-        //                     '<img src="assets/img/ajax-loader.gif"> loading...</p>');      
-        //            }
-        //
-        //        }
         return this;
     };
 });
